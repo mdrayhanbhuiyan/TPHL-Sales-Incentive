fa91d96a816644a7a072e4fc243e45cd6e4cd818
Md Rayhan Bhuiyan <rayhanbhuiyan2021@gmail.com>
2026-06-17 09:40:47 +0600

    Remove unused function exports from db.ts
    Removed unused function exports from db.ts.

--- src/server/db.ts
+++ src/server/db.ts
@@ -52,12 +52,3 @@ try {
   console.error("[db.ts] Failed to initialize Firebase:", err);
 }
 
-export async function getStore() {}
-export async function writeStore() {}
-export async function logAction() {}
-export async function recalculateAllIncentives() {}
-export async function recalculateAllIncentivesDirect() {}
-export async function initFirestore() {}
-export async function getFirebaseDiagnostics() {}
-export async function addNotification() {}
-export async function getLiveFirestoreBackup() {}
